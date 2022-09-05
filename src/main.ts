b06d59584fc212db1a372a6b7d2cf29648a9ec7c
Karuma <info@karuma.de>
2022-09-05 14:09:41 +0200

    Cleanup and bug-fixing

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,33 @@
-import './style.css'
+import './assets/style.css'
 import {Visualizer} from "./Visualizer";
-
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-`
 
 const file = document.getElementById("file-input") as HTMLInputElement;
 const canvas = document.getElementById("canvas") as HTMLCanvasElement;
-const h3 = document.getElementById('name')
+const main = document.getElementById("main") as HTMLElement;
+const title = document.getElementById('name')
 const audio = document.getElementById("audio") as HTMLAudioElement;
 
+const resize = () => {
+    if(canvas.parentNode) {
+        const rect = (canvas.parentNode as HTMLElement).getBoundingClientRect();
+        canvas.width = rect.width;
+        canvas.height = rect.height;
+    }
+}
+
 if (canvas) {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+    if(main) {
+        window.onresize = resize
+        resize()
+    }
     const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
     if (file) file.onchange = () => {
-        console.log('file changed')
         const files = file.files; // FileList containing File objects selected by the user (DOM File API)
         if (files) {
-            console.log('FILES[0]: ', files[0])
             audio.src = URL.createObjectURL(files[0]); // Creates a DOMString containing the specified File object
-            if (h3) {
+            if (title) {
                 const name = files[0].name
-                h3.innerText = `${name}` // Sets <h3> to the name of the file
+                title.innerText = `${name}` // Sets <h3> to the name of the file
             }
             const visualizer = new Visualizer(audio, ctx)
             visualizer.start()
